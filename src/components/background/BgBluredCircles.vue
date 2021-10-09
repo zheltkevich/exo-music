9d999ad7c129601418d07a5a6503b1dbfdd2683f
Aleksandr <Aleksandr>
2021-10-09 15:24:25 +0300

    Update main view

---
+++ src/components/background/BgBluredCircles.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="bg-blured-circles">
+        <!-- <div class="blured-circles__item blured-circles__item--1"></div> -->
+        <div class="bg-blured-circles__item bg-blured-circles__item--2"></div>
+        <div class="bg-blured-circles__item bg-blured-circles__item--3"></div>
+        <div class="bg-blured-circles__item bg-blured-circles__item--4"></div>
+        <div class="bg-blured-circles__item bg-blured-circles__item--1"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BgBluredCircles',
+};
+</script>
+
+<style lang="scss">
+.bg-blured-circles {
+    position: absolute;
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+    height: 100%;
+    opacity: 0.08;
+    filter: blur(80px);
+
+    &__item {
+        flex-basis: 50%;
+        flex-grow: 1;
+        flex-shrink: 0;
+        min-width: 100px;
+        min-height: 100px;
+        border-radius: 50%;
+
+        &--1 {
+            background-color: var(--clr-green);
+        }
+
+        &--2 {
+            background-color: var(--clr-purple);
+        }
+
+        &--3 {
+            background-color: var(--clr-orange);
+        }
+
+        &--4 {
+            background-color: var(--clr-blue-light);
+        }
+
+        &--5 {
+            background-color: var(--clr-blue);
+        }
+    }
+}
+</style>
---
+++ src/components/background/BgOrbits.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="bg-orbits">
+        <div class="bg-orbits__item bg-orbits__item--1"></div>
+        <div class="bg-orbits__item bg-orbits__item--2"></div>
+        <div class="bg-orbits__item bg-orbits__item--3"></div>
+        <div class="bg-orbits__item bg-orbits__item--4"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BgOrbits',
+};
+</script>
+
+<style lang="scss">
+.bg-orbits {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+
+    &__item {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        border: 1px solid var(--clr-elements);
+        border-radius: 50%;
+        transform: translate(-50%, -50%);
+        opacity: 0.2;
+
+        &--1 {
+            width: 620px;
+            height: 620px;
+        }
+
+        &--2 {
+            width: 720px;
+            height: 720px;
+        }
+
+        &--3 {
+            width: 810px;
+            height: 810px;
+        }
+
+        &--4 {
+            width: 1130px;
+            height: 1130px;
+        }
+    }
+}
+</style>
