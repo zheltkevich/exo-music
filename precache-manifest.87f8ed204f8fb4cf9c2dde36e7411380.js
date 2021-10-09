66d137e8bbca6a55b62b2b07a0fc9380679a3523
zheltkevich <zheltkevich42@gmail.com>
2021-10-09 15:06:34 +0000

    gh-pages

---
+++ precache-manifest.87f8ed204f8fb4cf9c2dde36e7411380.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bf2a1c902da842e994e",
+    "url": "/exo-music/css/app.27231a13.css"
+  },
+  {
+    "revision": "27e3649bab7c62fa21b8837c4842e40e",
+    "url": "/exo-music/fonts/Montserrat-Black.27e3649b.ttf"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/exo-music/fonts/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "19ba7aa52a78c3896558ac1c0a5fb4c7",
+    "url": "/exo-music/fonts/Montserrat-ExtraBold.19ba7aa5.ttf"
+  },
+  {
+    "revision": "570a244cacd3d78b8c75ac5dd622f537",
+    "url": "/exo-music/fonts/Montserrat-ExtraLight.570a244c.ttf"
+  },
+  {
+    "revision": "409c7f79a42e56c785f50ed37535f0be",
+    "url": "/exo-music/fonts/Montserrat-Light.409c7f79.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/exo-music/fonts/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/exo-music/fonts/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c641dbee1d75892e4d88bdc31560c91b",
+    "url": "/exo-music/fonts/Montserrat-SemiBold.c641dbee.ttf"
+  },
+  {
+    "revision": "43dd5b7a3d277362d5e801e5353e3a01",
+    "url": "/exo-music/fonts/Montserrat-Thin.43dd5b7a.ttf"
+  },
+  {
+    "revision": "237d04c0d0b97cef911c8cb9f4c54c51",
+    "url": "/exo-music/img/logo.237d04c0.svg"
+  },
+  {
+    "revision": "07687dd99bcc793b2bc08d04a0540573",
+    "url": "/exo-music/img/picture.07687dd9.png"
+  },
+  {
+    "revision": "15d77a9f3e05fc3634338c2a4f47dafb",
+    "url": "/exo-music/index.html"
+  },
+  {
+    "revision": "1bf2a1c902da842e994e",
+    "url": "/exo-music/js/app.7673dc27.js"
+  },
+  {
+    "revision": "206210358ee24b13599f",
+    "url": "/exo-music/js/chunk-vendors.a981af68.js"
+  },
+  {
+    "revision": "1da3d493f414e9720d0d6f80fd862035",
+    "url": "/exo-music/manifest.json"
+  }
+]);
