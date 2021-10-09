ba03ecb552fe5502914fbb00f9f1faaf35cb9ec4
Aleksandr <Aleksandr>
2021-10-09 13:28:12 +0300

    Update

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
+import MainView from '../views/MainView.vue';
 
 const routes = [
     {
         path: '/',
-        name: 'Home',
-        component: Home,
+        name: 'MainView',
+        component: MainView,
     },
 ];
 
